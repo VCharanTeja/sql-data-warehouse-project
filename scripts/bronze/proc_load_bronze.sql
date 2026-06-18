f1f517b993147ac6c4236703b92c4c08d95cdbfc
VCharanTeja <51997283+VCharanTeja@users.noreply.github.com>
2026-06-18 18:35:20 -0500

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -13,7 +13,7 @@ Parameters:
 	  This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
-    EXEC bronze.load_bronze;
+    CALL bronze.load_bronze();
 ===============================================================================
 */
 CREATE OR REPLACE PROCEDURE bronze.load_bronze()
